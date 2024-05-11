a18da8711103cfe2df0622cb476e2c7e49a7efa8
Abdulaziz834 <abdullaevabdulaziz123@gmail.com>
2024-05-11 23:32:55 +0500

    theme colors fix

--- script.js
+++ script.js
@@ -4,17 +4,40 @@ const titleTime = document.querySelector("[data-time]"),
 
 function updateDateTime() {
     let now = new Date();
-    titleDate.textContent = now.toLocaleDateString("en-uk", {weekday: "long", year: "numeric", month: "long", day: "numeric"})
+    titleDate.textContent = now.toLocaleDateString("en-uk", { weekday: "long", year: "numeric", month: "long", day: "numeric" })
     titleTime.textContent = now.toLocaleTimeString("en-uk")
 }
 
-function updateTime() {
+function updateTime(hideRest = false) {
     let now = new Date();
     titleTime.textContent = now.toLocaleTimeString("en-uk")
+    if (!hideRest) return
+    titleDate.style.display = "none";
 }
 
-updateDateTime()
+function updateDate() {
+    let now = new Date();
+    titleDate.textContent = now.toLocaleDateString("en-uk", { weekday: "long", year: "numeric", month: "long", day: "numeric" })
+    titleTime.style.display = "none";
+}
+
+
 
 window.addEventListener("load", e => {
-    let timeUpdater = setInterval(updateTime, 1000)
+    if (new URLSearchParams(window.location.search).get("time") === "") {
+        updateTime(hideRest = true)
+        setInterval(updateTime, 1000)
+    }
+    else if (new URLSearchParams(window.location.search).get("date") === "") {
+        updateDate()
+    }
+    else {
+        updateDateTime()
+        setInterval(updateTime, 1000)
+    }
+    let meta = document.createElement("meta")
+    meta.setAttribute("name", "theme-color")
+    meta.setAttribute("content", getComputedStyle(document.body).getPropertyValue("--clr-bg-body"))
+    document.head.appendChild(meta)
 })
+
